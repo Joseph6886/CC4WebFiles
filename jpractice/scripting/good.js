48287214cc24fe82f25a73d98c5009e305ee64c1
Joseph6886 <dragon6886@msn.com>
2017-08-30 09:51:24 -0400

    unloaded and reloaded bootstrap

---
+++ jpractice/scripting/good.js
@@ -0,0 +1,20 @@
+
+
+function change() {
+  document.images[0].src="../coffeeshop/pix3/brittany.jpg";
+
+}
+
+function change2() {
+  document.images[0].src="../coffeeshop/pix3/aguilera.jpg"
+
+}
+
+function world() {
+  document.images[1].src="../coffeeshop/pix3/christina.jpg"
+
+}
+function world2() {
+  document.images[1].src="../coffeeshop/pix3/Chistina.jpg"
+
+}
---
+++ jpractice/scripting/what.js
@@ -0,0 +1,20 @@
+
+
+function changeStatus() {
+
+var curMessage = "good1";
+function changeMessage() {
+
+  if (curMessage)== "good2" {
+    document.images[0].src="../coffeeshop/pix3/gwen6.jpg";
+    curMessage= "good1";
+  }
+else {
+  document.images[0].src=".../coffeeshop/pix3/pie1.jpg";
+  durMessage= "good2";
+}
+}
+
+
+
+}
